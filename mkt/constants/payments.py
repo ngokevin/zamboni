b97487565c324052956cc7bca504ec3d1c706df0
Kevin Ngo <ngoke@onid.oregonstate.edu>
2013-02-06 20:07:21 -0800

    use getrefundstatus if transaction is marked pending refund (bug 821900)

--- mkt/constants/payments.py
+++ mkt/constants/payments.py
@@ -1,19 +1,14 @@
-from tower import ugettext_lazy as _lazy
+from tower import ugettext as _, ugettext_lazy as _lazy
 
 
-STATUS_PENDING = 0  # When the payment has been started.
-STATUS_COMPLETED = 1  # When the IPN says its ok.
-STATUS_CHECKED = 2  # When someone calls pay-check on the transaction.
-# When we we've got a request for a payment, but more work needs to be done
-# before we can proceed to the next stage, pending.
-STATUS_RECEIVED = 3
-# Something went wrong and this transaction failed completely.
-STATUS_FAILED = 4
-# Explicit cancel action.
-STATUS_CANCELLED = 5
-
-STATUS_DEFAULT = STATUS_PENDING
-
+PENDING = 'PENDING'
+COMPLETED = 'OK'
+FAILED = 'FAILED'
+REFUND_STATUSES = {
+    PENDING: _('Pending'),
+    COMPLETED: _('Completed'),
+    FAILED: _('Failed'),
+}
 
 PROVIDER_PAYPAL = 0
 PROVIDER_BANGO = 1
