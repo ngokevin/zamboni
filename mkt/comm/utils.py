4482bd15451bb6b122cac6acf42cd9d8784bbd8c
Kevin Ngo <ngoke@mozilla.com>
2014-01-13 21:04:14 -0800

    create comm notes from reviewer tools on reviews

--- mkt/comm/utils.py
+++ mkt/comm/utils.py
@@ -175,7 +175,7 @@ def send_mail_comm(note):
 
 
 def create_comm_note(app, version, author, body, note_type=comm.NO_ACTION,
-                     perms=None):
+                     perms=None, no_switch=False):
     """
     Creates a note on an app version's thread.
     Creates a thread if a thread doesn't already exist.
@@ -191,7 +191,7 @@ def create_comm_note(app, version, author, body, note_type=comm.NO_ACTION,
              (e.g. {'developer': False, 'staff': True}).
 
     """
-    if not waffle.switch_is_active('comm-dashboard'):
+    if not no_switch and waffle.switch_is_active('comm-dashboard'):
         return None, None
 
     # Dict of {'read_permission_GROUP_TYPE': boolean}.
